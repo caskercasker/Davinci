68f1b9833b99a44041d519637978f3721bed4580
Sujin Lee <sjnu21@naver.com>
2020-01-03 12:17:02 +0900

    머지 잘못된 것 해결

--- DaVinciCode/src/Avatar.java
+++ DaVinciCode/src/Avatar.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
-
-import javax.swing.JPanel;
-//test
-public class Avatar extends JPanel{
-	Image back;
-	Avatar(){
-		setLayout(null); //ê¸°ë³¸ ë ˆì´ì•„ì›ƒ ë¬´ì‹œ
-		back = Toolkit.getDefaultToolkit().getImage("image\\game_bg.jpg");
-
-	}
-
-	//JButton btn_choice;
-	//java.awt.image.BufferedImage buttonIcon = ImageIO.read(new File("c:\\image\\avartar_4.jpg"));
-
-	//BufferedImage buttonIcon = ImageIO.read(new File("buttonIconPath"));
-	//button = new JButton(new ImageIcon(buttonIcon));
-
-
-	@Override
-	protected void paintComponent(Graphics g) { //ìŠ¤í‚¨ ìž…íž ë•Œ , ë°±ê·¸ë¼ìš´ë“œì— ì‚¬ìš©
-		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
-	}
-
-}
-=======
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -39,19 +10,19 @@ public class Avatar extends JPanel{
 	JButton b1,b2,b3,b4,b5;
 	Image back;
 	Avatar(){
-		setLayout(null); //±âº» ·¹ÀÌ¾Æ¿ô ¹«½Ã
+		setLayout(null); 
 		back = Toolkit.getDefaultToolkit().getImage("c:\\image\\gameground.jpg");
 		Image image;
 		
 		b1=new JButton(new ImageIcon("c:\\image\\_11.jpg"));
-		ImageIcon pressedIcon1 = new ImageIcon("c:\\image\\_111.jpg");   //´­·¶À»¶§ ÀÌ¹ÌÁö º¯°æ ¾ÆÁ÷ ¹Ì±¸Çö
+		ImageIcon pressedIcon1 = new ImageIcon("c:\\image\\_111.jpg");   //ëˆŒë €ì„ë•Œ ì´ë¯¸ì§€ ë³€ê²½ ì•„ì§ ë¯¸êµ¬í˜„
 		b2=new JButton(new ImageIcon("c:\\image\\_22.jpg"));
 		ImageIcon pressedIcon2 = new ImageIcon("c:\\image\\");
 		b3=new JButton(new ImageIcon("c:\\image\\_33.jpg"));
 		ImageIcon pressedIcon3 = new ImageIcon("c:\\image\\");
 		b4=new JButton(new ImageIcon("c:\\image\\_44.jpg"));
 		ImageIcon pressedIcon4 = new ImageIcon("c:\\image\\");
-		b5=new JButton("°áÁ¤");
+		b5=new JButton("ê²°ì •");
 		
 		
 		
@@ -76,9 +47,9 @@ public class Avatar extends JPanel{
 
 
 	@Override
-	protected void paintComponent(Graphics g) { //½ºÅ² ÀÔÈú ¶§ , ¹é±×¶ó¿îµå¿¡ »ç¿ë
+	protected void paintComponent(Graphics g) { 
 		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
 	}
 
 }
->>>>>>> 4c870910641765ad05bde48b2ec37574eb0cf8d4
+
