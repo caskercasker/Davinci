32dd3f71c2b1c3bcddbcd5424c5e1bac4fcdbea7
caskercasker <casketcasker@gmail.com>
2020-01-20 02:36:56 +0900

    check Exception

--- DaVinciCode/src/com/sist/server/Room.java
+++ DaVinciCode/src/com/sist/server/Room.java
@@ -1,21 +1,23 @@
-
 package com.sist.server;
 import java.util.Vector;
 
 public class Room {
 	String roomName,roomState,roomPwd;
 	int current,maxcount;
-	int ready=0;
+	int ready;
+	int gameTurn;
+
 	Vector<Server.Client> userVc = new Vector<Server.Client>();
 	public Room(String roomName, String roomState, String roomPwd, int maxcount) {
 		this.roomName = roomName;
 		this.roomState = roomState;
 		this.roomPwd = roomPwd;
 		this.maxcount = maxcount;
-		//this.ready =ready;
 		current = 1;
+		ready = 0;
+		this.gameTurn = 0;
+		
 	}
 
 
 }
-
