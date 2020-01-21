6aeae6442acc1f827697e215674e618300d56188
Rummikub <dottop626@gmail.com>
2020-01-21 16:31:34 +0900

    Makeroom:  delete password field

--- DaVinciCode/src/com/sist/client/MakeRoom.java
+++ DaVinciCode/src/com/sist/client/MakeRoom.java
@@ -13,28 +13,27 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class MakeRoom extends JDialog implements ActionListener{
-	JLabel la1,la2,la3,la4;
+	JLabel la1,la2,la3;
 	JTextField tf;
 	JComboBox box;
 	JPasswordField pf;
-	JRadioButton rb1,rb2;
+	JRadioButton rb1;
 	JButton b1,b2;
 	public MakeRoom() {
 		la1 = new JLabel("방이름",JLabel.RIGHT);
 		la2 = new JLabel("상태",JLabel.RIGHT);
 		la3 = new JLabel("인원",JLabel.RIGHT);
-		la4 = new JLabel("비밀번호");
+
 
 		tf = new JTextField();
 		pf = new JPasswordField();
 		rb1 = new JRadioButton("공개");
-		rb2 = new JRadioButton("비공개");
+
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(rb1);
-		bg.add(rb2);
+	
 		// 선택
 		rb1.setSelected(true);
-		la4.setVisible(false);
 		pf.setVisible(false);
 
 		b1 = new JButton("방만들기");
@@ -52,9 +51,7 @@ public class MakeRoom extends JDialog implements ActionListener{
 
 		la2.setBounds(10, 50, 50, 30);
 		rb1.setBounds(65, 50, 70, 30);
-		rb2.setBounds(140, 50, 70, 30);
-
-		la4.setBounds(65, 85, 55, 30);
+	
 		pf.setBounds(120, 85, 95, 30);
 
 		la3.setBounds(10, 120, 50, 30);
@@ -66,16 +63,15 @@ public class MakeRoom extends JDialog implements ActionListener{
 
 		p.setBounds(10, 160, 205, 35);
 		add(la1); add(tf);
-		add(la2); add(rb1); add(rb2);
+		add(la2); add(rb1);
 		add(la3); add(box);
-		add(la4); add(pf);
 		add(p);
 		//setSize(255, 255);
 		setBounds(448,156, 255, 255);
 		//setVisible(true);
 
 		rb1.addActionListener(this);
-		rb2.addActionListener(this);
+
 	}
 
 	public static void main(String[] args) {
@@ -87,15 +83,9 @@ public class MakeRoom extends JDialog implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource() == rb1) {
-			la4.setVisible(false);
+
 			pf.setVisible(false);
 			pf.setText("");
-
-		}else if(e.getSource() == rb2) {
-			la4.setVisible(true);
-			pf.setVisible(true);
-			pf.setText("");
-			pf.requestFocus();
 		}
 	}
 
